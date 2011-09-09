0d278075ef1020490f01f2b8584ca98e297da332
Matti Kotsalainen <matti@kotsalainen.org>
2011-09-09 21:53:52 +0200

    added install_autojump.sh

---
+++ install_autojump.sh
@@ -0,0 +1,7 @@
+wget https://github.com/downloads/joelthelion/autojump/autojump_v16.tar.gz
+tx autojump_v16.tar.gz
+cd autojump_v16
+sudo ./install.zsh
+echo "source /etc/profile.d/autojump.zsh" >> ~/.zshrc
+cd
+rm -rf autojump_v16
