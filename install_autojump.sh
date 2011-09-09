f9e4669d0abc01106321ce83317a310d58e92dd7
Matti Kotsalainen <matti@kotsalainen.org>
2011-09-09 23:38:48 +0200

    improoove

--- install_autojump.sh
+++ install_autojump.sh
@@ -1,5 +1,5 @@
 wget https://github.com/downloads/joelthelion/autojump/autojump_v16.tar.gz
-tx autojump_v16.tar.gz
+tar -xvzf autojump_v16.tar.gz
 cd autojump_v16
 sudo ./install.zsh
 echo "source /etc/profile.d/autojump.zsh" >> ~/.zshrc
