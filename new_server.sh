04c1413266122f21814ba096553f7602086a466b
Matti Kotsalainen <matti@kotsalainen.org>
2013-05-09 22:40:26 +0200

    more work on new_server

--- new_server.sh
+++ new_server.sh
@@ -1,15 +1,13 @@
+cd ~
+
 echo "installing nice utils"
-sudo apt-get install -y git-core zsh curl htop vim ctags ack-grep tig
+sudo apt-get install -y git-core zsh curl htop vim ctags ack-grep tig make
 
 echo "installing oh my zsh"
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
-#echo "installing vim dotfiles"
-#wget http://github.com/mkotsalainen/dotvim/raw/master/install.sh -O - | sh
-#sudo chsh $(pwd | sed 's|/home/||') -s /bin/zsh
-
 echo "getting utils"
-git clone git://github.com/mkotsalainen/utils.git ~/utils
+git clone https://github.com/mkotsalainen/utils.git ~/utils
 
 echo "install custom zsh stuff"
 ln -s ~/utils/matti.zsh ~/.oh-my-zsh/custom/
@@ -19,7 +17,13 @@ echo "installing Vundle. do :BundleInstall to download vim plugins"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s ~/utils/.vimrc ~
 
-#echo "installing autojump"
-#~/utils/install_autojump.sh
+echo "Installing fasdd"
+wget https://github.com/clvv/fasd/archive/1.0.1.tar.gz
+tar xvf 1.0.1.tar.gz
+cd fasd-1.0.1/
+sudo make install
+cd ..
+rm -rf fasd-1.0.1/
+
 
 echo "all done!"
